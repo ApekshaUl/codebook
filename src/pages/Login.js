c3455f326e475fbe3c323b12ea424172227cd0c5
ApekshaUl <121995087+ApekshaUl@users.noreply.github.com>
2025-03-05 12:44:31 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -25,8 +25,8 @@ export const Login = () => {
   }
   
   async function handleLoginGuest(){
-    email.current.value = "Apeksha@example.com";
-    password.current.value ="apeksha";
+    email.current.value = process.env.REACT_APP_GUEST_LOGIN;
+    password.current.value = process.env.REACT_APP_GUEST_PASSWORD;
     try{
       const authDetail = {
         email: email.current.value,
